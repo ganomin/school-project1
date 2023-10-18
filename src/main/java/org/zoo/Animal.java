01b01aadd6be9bf81bfaa6291830e438ea4294c5
ganomin <szymon.wirkus12@gmail.com>
2023-10-18 14:47:27 +0200

    lesson 3 part 2
    Signed-off-by: ganomin <szymon.wirkus12@gmail.com>

--- src/main/java/org/zoo/Animal.java
+++ src/main/java/org/zoo/Animal.java
@@ -3,8 +3,9 @@ package org.zoo;
 public class Animal {
     String specie;
     Long caloriesCounter = 0L;
-    Boolean isHungry = true;
-    Integer caloriesToFull;
+//    Boolean isHungry = true;
+    Boolean isAlive = true;
+//    Integer caloriesToFull;
 
     public Animal(String specie) {
         this.specie = specie;
@@ -24,9 +25,19 @@ public class Animal {
 //    }
 
     public void feed(Food food) {
-        System.out.println("Jem sobie: "
-                + food.foodType + " która ma " + food.calories + " kalorii.");
-        caloriesCounter += food.calories;
-        System.out.println("Dziś zjadłem " + caloriesCounter + " kalorii.");
+        if (food.foodType.equals("trutka")) {
+            System.out.println("Zjadłem trutkę i umieram uwu");
+            isAlive = false;
+        }
+
+        if (!isAlive) {
+            System.out.println("I'm dead");
+        }
+        else {
+            System.out.println("Jem sobie: "
+                    + food.foodType + " która ma " + food.calories + " kalorii.");
+            caloriesCounter += food.calories;
+            System.out.println("Dziś zjadłem " + caloriesCounter + " kalorii.");
+        }
     }
 }
