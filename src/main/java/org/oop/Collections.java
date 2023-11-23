0893b765a22387af77cfeca6c10f3b9a264237e9
ganomin <szymon.wirkus12@gmail.com>
2023-11-23 08:46:59 +0100

    i have no idea what im copy pasting lmao, please let me out
    Signed-off-by: ganomin <szymon.wirkus12@gmail.com>

---
+++ src/main/java/org/oop/Collections.java
@@ -0,0 +1,10 @@
+package org.oop;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Collections {
+    static List<String> lista = new ArrayList<>();
+    static List<String> lista2 = Arrays.asList("aa","bb");
+}
